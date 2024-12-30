65ae8b900300ec0d2589f557c1bf773fc89d83f3
aditya-students <xa03adityabhosale@gmail.com>
2024-12-30 18:55:26 +0530

    Update Chatuser.jsx

--- Frontend/src/home/Rightpart/Chatuser.jsx
+++ Frontend/src/home/Rightpart/Chatuser.jsx
@@ -14,22 +14,22 @@ function Chatuser() {
 
   // console.log(selectedConversation.fullname);
   return (
-    <div className="relative flex items-center h-[8%] justify-center gap-4 bg-slate-800 hover:bg-slate-700 duration-300 rounded-md">
+    <div className="relative flex items-center h-[8%] justify-center gap-4 bg-blue-200 hover:bg-blue-400 duration-300 rounded-md">
       <label
         htmlFor="my-drawer-2"
         className="btn btn-ghost drawer-button lg:hidden absolute left-5"
       >
         <CiMenuFries className="text-white text-xl" />
       </label>
-      <div className="flex space-x-3 items-center justify-center h-[8vh] bg-gray-800 hover:bg-gray-700 duration-300">
+      <div className="flex space-x-3 items-center justify-center h-[8vh] bg-blue-200 hover:bg-blue-400 duration-300 cursor-pointer">
         <div className="avatar online">
-          <div className="w-16 rounded-full">
+          <div className="w-12 rounded-full">
             <img src={profile} />
           </div>
         </div>
         <div>
-          <h1 className="text-xl">{selectedConversation.fullname}</h1>
-          <span className="text-sm">
+          <h1 className="text-xl text-black">{selectedConversation.fullname}</h1>
+          <span className="text-sm text-black">
             {getOnlineUsersStatus(selectedConversation._id)}
           </span>
         </div>
@@ -39,3 +39,4 @@ function Chatuser() {
 }
 
 export default Chatuser;
+
